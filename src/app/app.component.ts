7f4c54e9e7489f30f2f2fb913f66f2f180d0f29f
volodymyr horbatenko <vhorbate@cisco.com>
2023-03-12 23:39:30 +0200

    httpClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {IProduct} from './models/product';
 import {products as data} from './data/products';
+import {ProductsService} from "./services/products.service";
+import {Observable, tap} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-app';
+  // products: IProduct[] = []
+  loading = false
+  products$: Observable<IProduct[]>
 
-  products: IProduct[] = data
+  constructor(private productService: ProductsService) {
+  }
+
+  ngOnInit(): void {
+    this.loading = true
+    this.products$=this.productService.getAll().pipe(
+      tap(() => this.loading = false)
+    )
+    // this.productService.getAll().subscribe(products => {
+    //   this.products = products
+    //   this.loading = false
+    // } )
+  }
 }
